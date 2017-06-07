9d585c9362667d83bd70cdbc8355e578a72d1338
Euihoon Seol <seoleuihoon@gmail.com>
2017-06-07 15:29:01 -0400

    Add a header

--- RunEclipseLuna.sh
+++ RunEclipseLuna.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# The purpose of this shell script; In case that the error 'The Eclipse executable launcher was unable to locate its companion shared library' occurs, you could run the following script to run the eclipse executable file on your desktop directory.
+#title           :RunEclipseLuna.sh
+#description     :This script will run the eclipse executable file from your desktop directory in case that the error 'The Eclipse executable launcher was unable to locate its companion shared library' occurs.
+#author		       :Euihoon Seol
+#date            :20170607
+#version         :0.1
+#usage		       :./RunEclipseLuna.sh
+#notes           :
+#bash_version    :4.3.48(1)-release
 
 # this sciprt inherits its environment variables $HOME from the parent by using export
 export ECLIPSE_HOME="$HOME/eclipse"
